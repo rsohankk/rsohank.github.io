95a617a8c6f31aaec5deee8058abcf8ca8f044cc
rsohankk <138805312+rsohankk@users.noreply.github.com>
2023-08-03 20:35:44 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,53 @@
+//detectiong Button Press
+
+var numberOfDrumButton=document.querySelectorAll(".drum").length;
+for(var i=0; i<numberOfDrumButton; i++){
+  document.querySelectorAll(".drum")[i].addEventListener("click", handleClick)
+  function handleClick(){
+    var buttonInnerHTML=this.innerHTML;
+    makeSound(buttonInnerHTML);
+    
+  }
+  //detecting keyboard press
+
+  document.addEventListener("keypress",function(event){
+    makeSound(event.key);
+  })
+}
+function makeSound(key){
+  switch (key) {
+    case "w":
+      var tom1=new Audio("sounds/tom-1.mp3");
+      tom1.play();
+      break;
+    case "a":
+      var tom2=new Audio("sounds/tom-2.mp3");
+      tom2.play();
+      break;
+    case "s":
+      var tom3=new Audio("sounds/tom-3.mp3");
+        tom3.play();
+        break;
+    case "d":
+      var tom4=new Audio("sounds/tom-4.mp3");
+      tom4.play();
+      break;
+    case "j":
+      var crash=new Audio("sounds/crash.mp3");
+      crash.play();
+      break;
+    case "k":
+      var snare=new Audio("sounds/snare.mp3");
+      snare.play();
+      break;
+    case "l":
+      var kickbase=new Audio("sounds/kick-bass.mp3");
+      kickbase.play();
+      break;          
+    default:
+      break;
+  }
+}
+
+// var audio=new Audio("sounds/tom-1.mp3");
+//     audio.play();
